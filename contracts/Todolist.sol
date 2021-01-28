f75293410aca7bbdab803028e139120ab86535d9
Kenneth <kyleabs20@gmail.com>
2021-01-27 20:48:19 -0800

    we move

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -3,4 +3,19 @@ pragma solidity ^0.5.0;
 contract TodoList {
     //TODO public declaration of taskCount which can be used anywhere
     uint public taskcount = 0;
+
+    //TODO: decalaring elements of different types for a Task
+    struct Task {
+        uint id;
+        string content;
+        bool complete;
+    }
+
+    mapping(uint => Task) public tasks; 
+
+    function createTodo(string memory _content) public {
+        
+    }
+
+
 }
